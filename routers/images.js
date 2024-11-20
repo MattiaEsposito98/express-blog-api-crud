9e5be262d59ec3d0502be63794a71b002ce90579
MattiaEsposito98 <esposito.matty@hotmail.it>
2024-11-20 15:40:48 +0100

    add ricerca sia tramite nome e id nel metodo show

--- routers/images.js
+++ routers/images.js
@@ -1,49 +1,62 @@
-const express = require ('express')
+const express = require('express')
 const router = express.Router()
-const posts = require ('../posts')
+const posts = require('../posts')
+
 
 //index 
-router.get ('/', (req,res) => {
+router.get('/', (req, res) => {
   const title = posts.map(post => post.title);
-  console.log (title)
-  res.json ({posts})
+  console.log(title)
+  res.json({ posts })
 })
 
 //show
-router.get ('/:slug', (req,res) => {
-  const slug = req.params.slug
-  const post = posts.find((dolce) => dolce.slug === slug)
-  console.log (`Nome dolce: ${slug}`)
-  res.json ({
-    Dolce: post
-  })
-}) 
+router.get('/:identifier', (req, res) => {
+  let identifier = req.params.identifier
+
+  if (!isNaN(identifier)) {
+    identifier = parseInt(identifier)
+    const post = posts.find((post) => post.id === identifier)
+    res.json({
+      Dolce: post
+    });
+  }
+  else {
+    const post = posts.find((post) => post.slug === identifier)
+    res.json({
+      Dolce: post
+    });
+  }
+
+  console.log(`Parametro dinamico: ${identifier}`)
+
+})
 
 //Store
-router.post ('/', (req,res) => {
-  console.log (`Creo dolce:`)
-  res.send ('Creo un nuovo dolce')
+router.post('/', (req, res) => {
+  console.log(`Creo dolce:`)
+  res.send('Creo un nuovo dolce')
 })
 
 // Update
 router.put('/:slug', (req, res) => {
-	const slug = req.params.slug
-  console.log (`Aggiorno dolce: ${slug}`)
-	res.send(`Aggiorno il dolce: ${slug}`)
+  const slug = req.params.slug
+  console.log(`Aggiorno dolce: ${slug}`)
+  res.send(`Aggiorno il dolce: ${slug}`)
 })
 
 // modify
 router.patch('/:slug', (req, res) => {
-	const slug = req.params.slug
-  console.log (`Modifico dolce: ${slug}`)
-	res.send(`Modifico il dolce: ${slug}`)
+  const slug = req.params.slug
+  console.log(`Modifico dolce: ${slug}`)
+  res.send(`Modifico il dolce: ${slug}`)
 })
 
 // destroy
 router.delete('/:slug', (req, res) => {
-	const slug = req.params.slug
-  console.log (`Elimino dolce: ${slug}`)
-	res.send(`Elimino il dolce: ${slug}`)
+  const slug = req.params.slug
+  console.log(`Elimino dolce: ${slug}`)
+  res.send(`Elimino il dolce: ${slug}`)
 })
 
 module.exports = router
