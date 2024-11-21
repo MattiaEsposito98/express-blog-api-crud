3f300f4b48ef8508f3f465619f670f76519d50dc
MattiaEsposito98 <esposito.matty@hotmail.it>
2024-11-21 17:30:10 +0100

    add file utilities con le funzioni

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,4 +1,5 @@
 const posts = require('../data/posts')
+const {validate, converter, converterForDestroy} = require('../data/utilities')
 let lastIndex = posts.at(-1).id
 
 
@@ -183,53 +184,3 @@ const postIndex = converterForDestroy(identifier,posts)
 module.exports = { index, show, store, update, modify, destroy }
 
 
-
-function validate(req) {
-  const { title, slug, content, image, tags } = req.body
-
-  // VALIDAZIONE DEI DATI
-  const errors = []
-
-  if (!title) {
-    errors.push('Title is required')
-  }
-
-   if (!slug) {
-  	errors.push('Slug is required')
-  }
-
-  if (!content) {
-  	errors.push('Content is required')
-  }
-
-  if (!image) {
-  	errors.push('Image is required')
-  }
-
-  if (!tags) {
-  	errors.push('Tags is required')
-  }
-
-  return errors
-}
-
-
-//Funzione per covertire identifier
-function converter(identifier, arrayMain) {
-  if (!isNaN(identifier)) {
-    identifier = parseInt(identifier)
-    return arrayMain.find((el) => el.id === identifier)  //Cerco per ID
-  } else {
-    return arrayMain.find((el) => el.slug === identifier) //Cerco per slug
-  }
-}
-
-
-function converterForDestroy(identifier, arrayMain) {
-  if (!isNaN(identifier)) {
-    identifier = parseInt(identifier)
-    return arrayMain.findIndex((el) => el.id === identifier)  //Cerco per ID
-  } else {
-    return arrayMain.findIndex((el) => el.slug === identifier) //Cerco per slug
-  }
-}
